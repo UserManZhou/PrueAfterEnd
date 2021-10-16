ec61e0c421374b456075307c8d89dd1604fe5ec0
ZH <2830634496@qq.com>
2021-10-16 17:19:00 +0800

    完成开发小说接口

--- Prue-Novel/src/main/java/com/gec/prue/controller/NovelController.java
+++ Prue-Novel/src/main/java/com/gec/prue/controller/NovelController.java
@@ -12,39 +12,112 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@RequestMapping("findNovel")
 public class NovelController {
 
     @Autowired(required = true)
     private NovelMapper novelMapper;
 
 
-    @GetMapping("findNovelLove")
+    @GetMapping("Love")
     @ResponseBody
     public List<Novel> findNovel(){
         return NovelUtil.getNovelList(1,novelMapper);
     }
 
-    @GetMapping("findNovelOriental")
+    @GetMapping("Oriental")
     @ResponseBody
     public List<Novel> findNovelOriental(){
         return NovelUtil.getNovelList(2,novelMapper);
     }
 
-    @GetMapping("findNovelReincarnation")
+    @GetMapping("Reincarnation")
     @ResponseBody
     public List<Novel> findNovelReincarnation(){
         return NovelUtil.getNovelList(3, novelMapper);
     }
 
-    @GetMapping("findNovelAncient")
+    @GetMapping("Ancient")
     @ResponseBody
     public List<Novel> findNovelAncient(){
         return NovelUtil.getNovelList(4, novelMapper);
     }
 
-    @GetMapping("findNovelMagicSchool")
+    @GetMapping("MagicSchool")
     @ResponseBody
     public List<Novel> findNovelMagicSchool(){
         return NovelUtil.getNovelList(5, novelMapper);
+    }
+
+    @GetMapping("swordsman")
+    @ResponseBody
+    public List<Novel> swordsman(){
+        return NovelUtil.getNovelList(6, novelMapper);
+    }
+
+    @GetMapping("modern")
+    @ResponseBody
+    public List<Novel> modern(){
+        return NovelUtil.getNovelList(7, novelMapper);
+    }
+
+    @GetMapping("newMartial")
+    @ResponseBody
+    public List<Novel> newMartial(){
+        return NovelUtil.getNovelList(8, novelMapper);
+    }
+
+    @GetMapping("tradition")
+    @ResponseBody
+    public List<Novel> tradition(){
+        return NovelUtil.getNovelList(9, novelMapper);
+    }
+
+    @GetMapping("queer")
+    @ResponseBody
+    public List<Novel> queer(){
+        return NovelUtil.getNovelList(10, novelMapper);
+    }
+
+    @GetMapping("reality")
+    @ResponseBody
+    public List<Novel> reality(){
+        return NovelUtil.getNovelList(11, novelMapper);
+    }
+
+    @GetMapping("workplace")
+    @ResponseBody
+    public List<Novel> workplace(){
+        return NovelUtil.getNovelList(12, novelMapper);
+    }
+
+    @GetMapping("brother")
+    @ResponseBody
+    public List<Novel> brother(){
+        return NovelUtil.getNovelList(13, novelMapper);
+    }
+
+    @GetMapping("push")
+    @ResponseBody
+    public List<Novel> push(){
+        return NovelUtil.getNovelList(14, novelMapper);
+    }
+
+    @GetMapping("live")
+    @ResponseBody
+    public List<Novel> live(){
+        return NovelUtil.getNovelList(15, novelMapper);
+    }
+
+    @GetMapping("youth")
+    @ResponseBody
+    public List<Novel> youth(){
+        return NovelUtil.getNovelList(16, novelMapper);
+    }
+
+    @GetMapping("youthLife")
+    @ResponseBody
+    public List<Novel> youthLife(){
+        return NovelUtil.getNovelList(17, novelMapper);
     }
 }
